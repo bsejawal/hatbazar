cffe4aa37308dc6cdf1331f1d2594087b1b92a54
Bhesh Raj Sejawal <bsejawal@gmail.com>
2013-05-29 08:55:38 +0545

    change Hello controller

---
+++ src/main/java/com/hatbazar/spring/HelloController.java
@@ -0,0 +1,16 @@
+package com.hatbazar.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello hellsfsadfasf world!");
+		return "hello";
+	}
+}
